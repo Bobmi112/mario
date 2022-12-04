ddc24c55bd09df67257bc4b24878ced19523846b
Bobmi112 <93891173+Bobmi112@users.noreply.github.com>
2022-12-04 11:07:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent("game");
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on("pose", gotPoses);
+}
+
+function draw() {
+	game();
+}
+
+function modelLoaded() {
+	console.log("modelLoaded");
+}
+
+function gotPoses(results) {
+if (results.length > 0) {
+noseX = results[0].pose.nose.x;	
+noseY = results[0].pose.nose.y;
+console.log("Nose X: " + noseX + " Nose Y: " + noseY);
+}
+}
